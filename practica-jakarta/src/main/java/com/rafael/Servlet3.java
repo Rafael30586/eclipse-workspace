ce3ea9d10d21baea71ce2f7af9fcd9d590958b13
Rafael305 <fraq86@gmail.com>
2026-05-15 10:06:43 -0300

    examinando cookies

---
+++ practica-jakarta/src/main/java/com/rafael/Servlet3.java
@@ -0,0 +1,33 @@
+package com.rafael;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class Servlet3 extends HttpServlet {
+	
+       
+    
+    public Servlet3() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie cookie = new Cookie("nombre","sonic");
+		cookie.setAttribute("habilidad","super velocidad");
+		cookie.setMaxAge(1000);
+		response.addCookie(cookie);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
